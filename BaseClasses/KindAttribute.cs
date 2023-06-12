01fa88db5752a2d70d03ecd850b333761ae40cd0
MingxinFei <122725923+MingxinFei@users.noreply.github.com>
2023-06-12 17:28:01 +0800

    Add files via upload

--- BaseClasses/KindAttribute.cs
+++ BaseClasses/KindAttribute.cs
@@ -1,42 +1,39 @@
-﻿using System;
+﻿// 基础类命名空间
+namespace CTMS.BaseClasses;
 
-// 基础类命名空间
-namespace CTMS.BaseClasses
+/// <summary>
+/// 分类特性类
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+[Kind("修饰特性")]
+public class KindAttribute : Attribute
 {
-    /// <summary>
-    /// 分类特性类
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-    [Kind("修饰特性")]
-    public class KindAttribute : Attribute
+    private string kind;
+    public string Kind
     {
-        private string kind;
-        public string Kind
+        get
         {
-            get
+            if (kind == null)
             {
-                if (kind == null)
-                {
-                    throw new UnifyException("检查到空对象", GetType());
-                }
-                return kind;
+                throw new UnifyException("检查到空对象", GetType());
             }
-            set
-            {
-                if (value == null) {
-                    throw new UnifyException("检查到空对象", GetType());
-                }
-                kind = value;
-            }
+            return kind;
         }
+        set
+        {
+            if (value == null) {
+                throw new UnifyException("检查到空对象", GetType());
+            }
+            kind = value;
+        }
+    }
 
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        /// <param name="inKind">分类文本</param>
-        public KindAttribute(string inKind)
-        {
-            Kind = inKind;
-        }
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="inKind">分类文本</param>
+    public KindAttribute(string inKind)
+    {
+        Kind = inKind;
     }
 }
